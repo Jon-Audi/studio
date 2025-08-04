14dcf148eb92609f22fbe845384628cfce149243
Jon Audiffred <jon.audiffred@gmail.com>
2025-08-04 18:29:50 +0000

    under Chain-link Fence Calculator can we add the option for 10 foot tall

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,5 +1,5 @@
 
-export const FENCE_HEIGHT_OPTIONS = ["3", "4", "5", "6", "7", "8"]; // in feet
+export const FENCE_HEIGHT_OPTIONS = ["3", "4", "5", "6", "7", "8", "9", "10"]; // in feet
 export const FENCE_TYPE_OPTIONS = ["residential", "commercial"];
 export const GATE_FRAME_DIAMETER_OPTIONS = ["1 3/8″", "1 5/8″", "2″"];
 export const GATE_TYPE_OPTIONS = ["Single", "Double"]; // Used by PipeCutCalculator
